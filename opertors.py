c7f0892522203a48ab0264693a72929a6d805a87
maggymbur <mburumaggy62@gmail.com>
2022-03-05 11:15:00 +0300

    add this file

---
+++ opertors.py
@@ -0,0 +1,27 @@
+from operator import truediv
+
+
+x=10
+y=5
+#arithmetic operators
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+#relational operators
+print(x==y)
+print(x<y)
+print(x>y)
+print(x!=y)
+#logical operators
+a= False
+b= True
+print(a and b)
+print(a or b)
+print(9//2) #prints no remeinder
+
+
+
+
+
