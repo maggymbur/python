5d5619d777e2ac9e8c453da7083adb80d6477a1d
maggymbur <mburumaggy62@gmail.com>
2022-03-05 13:01:22 +0300

    update tis file

---
+++ statements.py
@@ -0,0 +1,22 @@
+#if statement
+a=10
+b=5
+if(a>b):
+    print("a is greater than b")
+
+#example 2
+i=10
+if(i>15):
+    print("10 is less than 15")
+print("i am not if")  
+
+#if else statement
+i=20
+if(i<15):
+    print("i is smaller than 15")
+    print("i'm in if block")
+else:
+    print("i is greater by 15")
+    print("i'm in else block")
+print("not in else block or if block")
+    
